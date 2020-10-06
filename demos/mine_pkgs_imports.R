2f17bdab0f47971450b18734f0b0190f5e1cd692
Marc Teunis <marc.teunis@hu.nl>
2020-10-06 19:02:08 +0200

    add geo data to /data/geo

--- demos/mine_pkgs_imports.R
+++ demos/mine_pkgs_imports.R
@@ -1,8 +1,8 @@
-## Problem: What R-package installed on the 
+## Problem: What R-package installed on the
 ## system has the most dependencies (as defined in the Imports' field)
 
 ## Plan
- 
+
 # Use google to: get the dependencies for R packages
 
 # 1) Which packages are installed currently on the system
@@ -12,36 +12,24 @@
 # 5) look at first row
 # 6) Make a graph
 
-# ad1) 
+# ad1)
 library(tidyverse)
 
 
 pkgs <- installed.packages() %>%
   as_tibble() %>%
-  dplyr::select(
-    Package,
-    Imports
-  ) %>%
-  mutate(
-    splitted = map(
-      .x = Imports,
-      .f = str_split, pattern = ","
-  )) %>%
-  mutate(
-    split_unlist = map(
-      .x = splitted,
-      .f = unlist)) %>%
-  mutate(
-    n_deps = map(
-      split_unlist,
-      length
-    ) %>% unlist) %>%
+  dplyr::select(Package,
+                Imports) %>%                                                       mutate(splitted = map(.x = Imports,
+                        .f = str_split, pattern = ",")) %>%
+  mutate(split_unlist = map(.x = splitted,
+                            .f = unlist)) %>%
+  mutate(n_deps = map(split_unlist,
+                      length) %>% unlist) %>%
   dplyr::filter(n_deps > 10) %>%
-  ggplot(aes(x = reorder(as_factor(Package), n_deps), y = n_deps)) + 
-  geom_point() + 
+  ggplot(aes(x = reorder(as_factor(Package), n_deps), y = n_deps)) +
+  geom_point() +
   coord_flip()
- 
+
 pkgs
 
 # pkgs$n_deps
-
