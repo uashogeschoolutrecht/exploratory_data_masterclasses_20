be1282e393a1956f3d14c0fbf1f135edf6a43f78
Marc Teunis <marc.teunis@hu.nl>
2021-02-11 13:41:35 +0100

    add chapter 6 lab1A

--- install_server_r_packages.R
+++ install_server_r_packages.R
@@ -3,6 +3,8 @@
 ## as root run in the Terminal
 # Rscript install_server_r_packages.R
 
+
+
 url <- "https://raw.githubusercontent.com/uashogeschoolutrecht/exploratory_data_masterclasses_20/master/DEPENDENCIES.txt"
 
 download.file(url = url, destfile = "DEPENDENCIES.txt")
@@ -11,9 +13,9 @@ lib_path <- .libPaths()[2]
 install.packages("tidyverse", lib = lib_path, dependencies = TRUE)
 pkgs <- readr::read_csv("DEPENDENCIES.txt")
 
-purrr::walk(
+pkgs_installed <- purrr::map(
   pkgs$package_name,
-  install.packages,
+  safely(install.packages),
   lib = lib_path,
   dependencies = TRUE
 )
